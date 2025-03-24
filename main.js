21c599e479f80cd2527db65c75215949130182a0
257m <257m@proton.me>
2025-03-24 12:57:14 -0400

    explosion

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
-let img;
-
-
 /** This is a setup function. */
 function setup() {
-  createCanvas(400, 400); 
-  img = loadImage('/assets/iso-64x64-outside.png');
-  
-
+  createCanvas(400, 400);
 }
 
 /** This is a draw function. */
 function draw() {
-  background(220);
-  rect(100, 100, 100, 100)
+	background(220);
+	rect(100, 100, 100, 100)
 
   // Draw the image.
   image(img, 0, 0);
