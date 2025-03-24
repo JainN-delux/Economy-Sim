ecee05b4f45d3b25f093c4db3556c44258a77053
Naitik Jain <naitik.jain07@gmail.com>
2025-03-24 12:53:36 -0400

    added images

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
+let img;
+
+
 /** This is a setup function. */
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 400); 
+  img = loadImage('/assets/iso-64x64-outside.png');
+  
+
 }
 
 /** This is a draw function. */
 function draw() {
   background(220);
   rect(100, 100, 100, 100)
+
+  // Draw the image.
+  image(img, 0, 0);
 
 }
