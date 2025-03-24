31ec0a8883b52540f8898a3e8355b9c6203a04b9
257m <257m@proton.me>
2025-03-24 12:57:14 -0400

    Frankensteined image together

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
+function preload() {
+	let tileset = loadImage("assets/tileset.png");
+}
+
 /** This is a setup function. */
 function setup() {
-  createCanvas(400, 400);
+	createCanvas(400, 400);
 }
 
 /** This is a draw function. */
 function draw() {
-  background(220);
-  rect(100, 100, 100, 100)
+	background(220);
+	rect(100, 100, 100, 100)
 
 }
