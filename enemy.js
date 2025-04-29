f5378eda9a9bb8d0563fabc3185292cceffb1ffd
Naitik Jain <naitik.jain07@gmail.com>
2025-04-29 08:12:28 -0400

    minor update to enemy.js

--- enemy.js
+++ enemy.js
@@ -1,13 +1,16 @@
 class Enemy { // The enemy class
-    constructor(tileX, tileY, currentHealth, maxHealth, damage, weapon, movement, value, level){
-        this.tileX = tileX; // which vertical tile is the player on
-        this.tileY = tileY; // which horizontal tile is the player on
-        this.currentHealth = currentHealth; // current hp
-        this.maxHealth = maxHealth; // the max health the player has
+    constructor(tileX, tileY, maxHealth, damage, weapon, movement, state){
+        this.tileX = tileX; // which vertical tile is the enemy on
+        this.tileY = tileY; // which horizontal tile is the enemy on
+        this.currentHealth = maxHealth; // current hp
+        this.maxHealth = maxHealth; // the max health the enemy has
         this.damage = damage; // base damage
         this.weapon = weapon; //future for diff tile attacks
         this.movement = movement; // tiles the enemy can move
-        this.value = value; // amount of gold dropped
-        this.level = level; // how strong the enemy is
+        this.state = "move"
+        // this.value = value; // amount of gold dropped
+        // this.level = level; // how strong the enemy is
     }
+
+    
 }
